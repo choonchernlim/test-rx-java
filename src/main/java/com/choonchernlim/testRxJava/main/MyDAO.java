c916e639481e356968d359327a0dcb7408e43555
Choon-Chern Lim <limc@mayo.edu>
2014-04-08 11:16:17 -0500

    Refactored code.

--- src/main/java/com/choonchernlim/testRxJava/main/MyDAO.java
+++ src/main/java/com/choonchernlim/testRxJava/main/MyDAO.java
@@ -1,49 +1,43 @@
 package com.choonchernlim.testRxJava.main;
 
+import com.google.common.collect.ImmutableMap;
 import static java.lang.Thread.sleep;
 
-import java.util.HashMap;
 import java.util.Map;
 
+
 public class MyDAO {
-    private static final int SLEEP_IN_SECOND = 2000;
+    private static final Map<Integer, String> USER_MAP = ImmutableMap.of(1, "(1) Mike",
+                                                                         2, "(2) Tom",
+                                                                         3, "(3) Jane",
+                                                                         4, "(4) Paula",
+                                                                         5, "(5) George");
 
-    private static final Map<Integer, String> userMap = new HashMap<Integer, String>();
-    private static final Map<Integer, Integer> pointMap = new HashMap<Integer, Integer>();
-
-    static {
-        userMap.put(1, "(1) Mike");
-        userMap.put(2, "(2) Tom");
-        userMap.put(3, "(3) Jane");
-        userMap.put(4, "(4) Paula");
-        userMap.put(5, "(5) George");
-
-        pointMap.put(1, 100);
-        pointMap.put(2, 200);
-        pointMap.put(3, 300);
-        pointMap.put(4, 400);
-        pointMap.put(5, 500);
-    }
+    private static final Map<Integer, Integer> POINT_MAP = ImmutableMap.of(1, 100,
+                                                                           2, 200,
+                                                                           3, 300,
+                                                                           4, 400,
+                                                                           5, 500);
 
     public String getUser(int personId) {
-        try {
-            sleep(SLEEP_IN_SECOND);
-        }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        return userMap.get(personId);
+        pause();
+        return USER_MAP.get(personId);
     }
 
     public Integer getPoint(int pointId) {
+        pause();
+        return POINT_MAP.get(pointId);
+    }
+
+    /**
+     * Simulating procedure call against DB, web services, etc.
+     */
+    private void pause() {
         try {
-            sleep(SLEEP_IN_SECOND);
+            sleep(2000);
         }
         catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        return pointMap.get(pointId);
     }
 }
